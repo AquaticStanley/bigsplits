a15f9dc21f4c9b75ab3fd7e9a11de04f72b059e0
RovinRover <ltwyr4@mst.edu>
2017-01-30 01:13:01 -0600

    Timer Polish

--- public/javascripts/timer.js
+++ public/javascripts/timer.js
@@ -1,11 +1,14 @@
 var time = 0;
 var startTime;
 var intervalTime = 10;
+var timer;
 
 
 function startTimer(){
-    startTime = new Date().getTime();
-    timer = setInterval(addTime, intervalTime);
+    if(timer == undefined){
+        startTime = new Date().getTime();
+        timer = setInterval(addTime, intervalTime);
+    }
 }
 
 function addTime(){
@@ -14,14 +17,19 @@ function addTime(){
 }
 
 function updateDisplay(){
-    sec = Math.floor((time%60000)/10);
-    min = Math.floor((time%3600000)/60000);
-    hour = Math.floor(time/3600000);
-    $("#timerDisplay").text("" + hour + ":" + min + ":" + sec);
+    sec = "" +  Math.floor((time%60000)/10);
+    while(sec.length < 4) sec = "0" + sec;
+    sec = sec.slice(0, 2) + "." + sec.slice(2);
+    min = "" + Math.floor((time%3600000)/60000);
+    while(min.length < 2) min = "0" + min;
+    hour = "" + Math.floor(time/3600000);
+    while(hour.length < 2) hour = "0" + hour;
+    $("#timerDisplay").text( hour + ":" + min + ":" + sec);
 }
 
 function stopTimer(){
     time = new Date().getTime() - startTime;
     clearInterval(timer);
+    timer = undefined;
     updateDisplay();
 }
