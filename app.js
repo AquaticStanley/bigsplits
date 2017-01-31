626a356bdbfd19f11e4c252ba28b695c3cc1385f
RovinRover <ltwyr4@mst.edu>
2017-01-31 15:59:06 -0600

    Added timer page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var timer = require('./routes/timer');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/timer', timer);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/timer.js
+++ public/javascripts/timer.js
@@ -2,29 +2,24 @@ var time = 0;
 var startTime;
 var intervalTime = 10;
 var timer;
+var splitCounter = 0;
+var splits = [10000, 20000, 30000, 40000];
 
 
 function startTimer(){
     if(timer == undefined){
         startTime = new Date().getTime();
-        timer = setInterval(addTime, intervalTime);
+        timer = setInterval(clockLoop, intervalTime);
     }
 }
 
-function addTime(){
+function clockLoop(){
     time = new Date().getTime() - startTime;
     updateDisplay();
 }
 
 function updateDisplay(){
-    sec = "" +  Math.floor((time%60000)/10);
-    while(sec.length < 4) sec = "0" + sec;
-    sec = sec.slice(0, 2) + "." + sec.slice(2);
-    min = "" + Math.floor((time%3600000)/60000);
-    while(min.length < 2) min = "0" + min;
-    hour = "" + Math.floor(time/3600000);
-    while(hour.length < 2) hour = "0" + hour;
-    $("#timerDisplay").text( hour + ":" + min + ":" + sec);
+     $("#timerDisplay").text(formatTime(time));
 }
 
 function stopTimer(){
@@ -32,4 +27,23 @@ function stopTimer(){
     clearInterval(timer);
     timer = undefined;
     updateDisplay();
+}
+
+function formatTime(p_time){
+    var sec = "" +  Math.floor((p_time%60000)/10);
+    while(sec.length < 4) sec = "0" + sec;
+    sec = sec.slice(0, 2) + "." + sec.slice(2);
+    var min = "" + Math.floor((time%3600000)/60000);
+    while(min.length < 2) min = "0" + min;
+    var hour = "" + Math.floor(time/3600000);
+    while(hour.length < 2) hour = "0" + hour;
+    return hour + ":" + min + ":" + sec;
+}
+
+function split(){
+    var split = new Date().getTime() - startTime;
+    $("splitTime" + splitCounter).text(split);
+    var splitDifference = split - splits[splitcounter];
+    $("splitDifference" + splitCounter).text()
+    splitCounter++;
 }
